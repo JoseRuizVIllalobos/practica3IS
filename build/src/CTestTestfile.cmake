02bbca5f53cbd0d84f0a08078db77d8ee4c44afe
JoseRuizVIllalobos <152162644+JoseRuizVIllalobos@users.noreply.github.com>
2023-12-19 08:23:48 +0100

    Add files via upload

---
+++ build/src/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/jose/Escritorio/IS/src
+# Build directory: /home/jose/Escritorio/IS/build/src
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("actividad")
+subdirs("estadisticas")
---
+++ build/src/actividad/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/jose/Escritorio/IS/src/actividad
+# Build directory: /home/jose/Escritorio/IS/build/src/actividad
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
